aaee3fc93233fbcc19f5e0400394eebc6d0e2ab8
Dang Nhat Huy <113001292+dang-nhat-huy@users.noreply.github.com>
2024-06-24 14:07:08 +0700

    WPF

---
+++ BadmintonRentingWPF/UI/wCustomer.xaml.cs
@@ -0,0 +1,126 @@
+﻿using BadmintonRentingBusiness;
+using BadmintonRentingData.DTO;
+using BadmintonRentingData.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BadmintonRentingWPF.UI
+{
+    /// <summary>
+    /// Interaction logic for wCustomer.xaml
+    /// </summary>
+    public partial class wCustomer : Window
+    {
+        private readonly ICustomerBusiness customerBusiness;
+        public wCustomer(ICustomerBusiness customerBusiness)
+        {
+            InitializeComponent();
+            this.customerBusiness = customerBusiness;
+            Loaded += Window_Loaded;
+        }
+
+        private void grdCustomer_ButtonDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void grdCustomer_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+        private void grdCustomer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (grdCustomer.SelectedItem is Customer selectedCustomer)
+            {
+                txtCustomerId.Text = selectedCustomer.CustomerId.ToString();
+                txtCustomerName.Text = selectedCustomer.CustomerName;
+                txtPhone.Text = selectedCustomer.Phone.ToString();
+                txtEmail.Text = selectedCustomer.Email;
+                txtIsStatus.Text = selectedCustomer.IsStatus;
+            }
+        }
+        private async void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                long customerId = long.Parse(txtCustomerId.Text);
+
+                var existcustomer = await customerBusiness.GetById(customerId);
+                var newCustomer = existcustomer.Data as Customer; //Doi tu kieu BusinessResult thanh Customer
+                if (newCustomer == null)                  
+                {                   
+                    var customerDTO = new CustomerRequestDTO
+                    {
+                        CustomerName = txtCustomerName.Text,
+                        Phone = int.Parse(txtPhone.Text),
+                        Email = txtEmail.Text,
+                        IsStatus = txtIsStatus.Text
+                    };
+
+                    var result = await customerBusiness.Create(customerDTO);
+                    MessageBox.Show(result.Message, "Save");
+                }
+                else
+                {
+                    var customerDTO = new CustomerRequestDTO
+                    {
+                        CustomerName = txtCustomerName.Text,
+                        Phone = int.Parse(txtPhone.Text),
+                        Email = txtEmail.Text,
+                        IsStatus = txtIsStatus.Text
+                    };
+                    
+                    var result = await customerBusiness.Update(customerId, customerDTO);
+                    MessageBox.Show(result.Message, "Update");
+                }
+
+                txtCustomerName.Text = string.Empty;
+                txtPhone.Text = string.Empty;
+                txtEmail.Text = string.Empty;
+                txtIsStatus.Text = string.Empty;
+                await this.LoadGrdCustomer();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error");
+            }
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            txtCustomerId.Text = string.Empty;
+            txtCustomerName.Text = string.Empty;
+            txtPhone.Text = string.Empty;
+            txtEmail.Text = string.Empty;
+            txtIsStatus.Text = string.Empty;
+        }
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            await LoadGrdCustomer();
+        }
+        private async Task LoadGrdCustomer()
+        {
+            var result = await customerBusiness.GetAll();
+
+            if (result.Status > 0 && result.Data != null)
+            {
+                grdCustomer.ItemsSource = result.Data as List<Customer>;
+            }
+            else
+            {
+                grdCustomer.ItemsSource = new List<Customer>();
+            }
+        }
+    }
+}
