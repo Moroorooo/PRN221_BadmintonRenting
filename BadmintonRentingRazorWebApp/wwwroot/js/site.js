3f4b725bbd302bd0327fa4b8ae51678c96b153f7
Dang Nhat Huy <113001292+dang-nhat-huy@users.noreply.github.com>
2024-07-15 12:51:29 +0700

    add comment

--- BadmintonRentingRazorWebApp/wwwroot/js/site.js
+++ BadmintonRentingRazorWebApp/wwwroot/js/site.js
@@ -1,5 +1,6 @@
-﻿"use strict";
+﻿"use strict";  // Sử dụng chế độ nghiêm ngặt của JavaScript để tránh một số lỗi thường gặp
 $(() => {
+    // Khởi tạo kết nối với SignalR hub tại địa chỉ '/signalrServer'
     var connection = new signalR.HubConnectionBuilder().withUrl("/signalrServer").build();
 
     connection.start().then(function () {
@@ -7,6 +8,7 @@ $(() => {
     }).catch(function (err) {
         console.error("SignalR Connection Error: ", err.toString());
     });
+    // Định nghĩa sự kiện khi nhận được tín hiệu 'LoadCustomer' từ server
     connection.on("LoadCustomer", function () {
         console.log("Received delete signal");
         location.reload();// Tải lại trang hiện tại
